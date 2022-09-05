272c6cafc6ce9cd1679c96364ad942cf5b9700d7
SkrChowdhury <sakkhor.chowdhury@gmail.com>
2022-09-05 11:41:04 +0600

    main dart file done

---
+++ android/app/src/main/kotlin/com/example/form_data_pass_using_provider_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.form_data_pass_using_provider_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/account.dart';
+import 'screens/settings.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Form with Provider',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Form with Provider'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: AccountScreen(), routes: {
+      'account_screen': (context) => AccountScreen(),
+      'settings_screen': (context) => SettingsScreen(),
+    });
+  }
+}
